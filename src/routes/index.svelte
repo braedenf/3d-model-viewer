1ea8d4c5eecc9fbda141b7ffcfb507e420b5021b
braedenf <braedenleefoster@gmail.com>
2021-08-29 17:02:07 +1200

    Referencing updated model for testing on ios

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,7 +11,7 @@
 
 	let modelType = 'LowBack';
 
-	$: myModel = cloudinary.image(`Carousel_${modelType}_Default`).toURL();
+	$: myModel = cloudinary.image(`Carousel_${modelType}_MaharamMeld-Antler`).toURL();
 
 	function loadTestModel() {
 		// myModel = cloudinary.image('Astronaut_Test').toURL();
