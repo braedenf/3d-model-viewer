aa8e0505f1e8440eb257dd7c597612b534dbc3dc
braedenf <braedenleefoster@gmail.com>
2021-09-17 15:38:57 +1200

    Added AR QR Code feature, need to figure out how to actually load model in AR though

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,6 +14,8 @@
 	import { Cloudinary } from '@cloudinary/url-gen';
 	import { onMount } from 'svelte';
 	import { panSkybox } from '$lib/pan-skybox';
+	import QrCode from '$lib/QRCode.svelte';
+	import Modal from '$lib/Modal.svelte';
 
 	/* 
 		Product Data is loaded from a local endpoint in json format
@@ -31,11 +33,8 @@
 		Selected model and selected model type for loading in from cloudinary dynamically
 	*/
 	let selectedModel = 0; // This is the product that gets loaded from cloudinary
-
 	let selectedModelType = 0; // This is the product variant
-
 	let selectedMaterial = 0; // This is the material
-
 	let modelMaterial = products[selectedModel].materials[selectedMaterial];
 
 	function updatedSelectedModelType(i) {
@@ -54,23 +53,30 @@
 		modelMaterial = products[selectedModel].materials[selectedMaterial];
 	}
 
-	let modelViewer; // Model viewer gets bound to model-viewer web component
+	// Adds spaces between capital letters
+	function addSpaceBetweenCapitals(word) {
+		return word.replace(/([A-Z])/g, ' $1').trim();
+	}
 
-	let isARCompatible; // Gets the ar-status and presents alt button if not available
+	let modelViewer; // Model viewer gets bound to model-viewer web component
 
 	/* 
 		Wait for modelViewer to be mounted
 	*/
+	let isARAvailable;
+	let qrModalOpen = false;
 
 	onMount(() => {
-		if (modelViewer.getAttribute('ar-status') == 'not-presenting') {
-			isARCompatible = false;
-		} else {
-			isARCompatible = true;
-		}
-
 		// For some reason I need to crank the exposure here???
 		modelViewer.exposure = 1.8;
+		// Gets the ar-status and presents alt button if not available
+		if (modelViewer.getAttribute('ar-status') == 'not-presenting') {
+			isARAvailable = false;
+			console.log(modelViewer.getAttribute('ar-status'));
+		} else {
+			isARAvailable = true;
+			console.log(modelViewer.getAttribute('ar-status'));
+		}
 	});
 
 	/* 
@@ -90,49 +96,69 @@
 		.toURL();
 </script>
 
-<div class="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:px-12 h-screen w-full mt-10">
+{#if qrModalOpen}
+	<Modal>
+		<h3 class="text-2xl text-center pt-3 font-headline">Scan to view in AR</h3>
+		<div class="w-full flex justify-center p-6">
+			<div class="h-60 w-60">
+				<QrCode text="https://determined-mirzakhani-828e94.netlify.app/" />
+			</div>
+		</div>
+		<div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+			<button
+				on:click={() => (qrModalOpen = false)}
+				type="button"
+				class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+			>
+				Exit
+			</button>
+		</div>
+	</Modal>
+{/if}
+
+<div class="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:px-12 w-full mt-10 lg:mt-20">
 	<div class="relative">
 		<model-viewer
 			bind:this={modelViewer}
 			use:panSkybox
-			class="relative h-[500px] lg:h-3/4 w-full bg-gray-200"
+			class="relative h-[40em] lg:h-full w-full bg-gray-200"
 			poster={loadedPoster}
 			src={loadedModel}
 			loading="auto"
-			alt="A comfy couch"
+			alt="3D Model Viewer"
 			ar
 			ar-modes="webxr scene-viewer quick-look"
 			ar-status
 			environment-image="./whiteroom2Windows_512.hdr"
-			exposure="1"
+			exposure="2"
 			auto-rotate
 			camera-controls
-			yaw="20deg"
 			shadow-intensity="2"
 		/>
 		<!-- Only show QR code AR button if not on a ar compatible device -->
-		{#if !isARCompatible}
-			<button
-				on:click={() => alert('TODO: Add QR Code for AR')}
-				class="rounded-full bg-gray-800 hover:bg-gray-600 w-8 h-8 flex justify-center items-center absolute top-0 right-0 mr-4 mt-4 shadow"
+		<button
+			on:click={() => (qrModalOpen = true)}
+			class="rounded-full bg-gray-800 hover:bg-gray-600 w-8 h-8 flex justify-center items-center absolute top-0 right-0 mr-4 mt-4 shadow {isARAvailable
+				? 'hidden'
+				: ''}"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6 text-gray-300"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
 			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-6 w-6 text-gray-300"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5"
-					/>
-				</svg>
-			</button>
-		{/if}
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5"
+				/>
+			</svg>
+		</button>
 	</div>
+
 	<div class="flex flex-wrap space-x-2 lg:order-last mr-4 items-center mb-10 mx-10">
 		{#each products as product, i}
 			<div class="flex flex-col space-y-2 content-center text-center">
@@ -146,7 +172,7 @@
 						alt={product.name}
 					/>
 				</button>
-				<h6 class="text-sm font-semibold">{product.name.replace(/([A-Z])/g, ' $1').trim()}</h6>
+				<h6 class="text-sm font-semibold">{addSpaceBetweenCapitals(product.name)}</h6>
 			</div>
 		{/each}
 	</div>
@@ -158,9 +184,7 @@
 					<div tabindex="0" class="btn btn-wide btn-ghost shadow-lg">
 						<div class="flex justify-between w-full items-center">
 							<h6>
-								{products[selectedModel].variants[selectedModelType]
-									.replace(/([A-Z])/g, ' $1')
-									.trim()}
+								{addSpaceBetweenCapitals(products[selectedModel].variants[selectedModelType])}
 							</h6>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -180,7 +204,7 @@
 						{#each products[selectedModel].variants as modelVariant, i}
 							<li>
 								<button class="btn btn-ghost" on:click={() => updatedSelectedModelType(i)}
-									>{modelVariant.replace(/([A-Z])/g, ' $1').trim()}</button
+									>{addSpaceBetweenCapitals(modelVariant)}</button
 								>
 							</li>
 						{/each}
@@ -189,11 +213,11 @@
 			</div>
 			<div class="lg:order-last lg:mt-20">
 				<h5 class="text-2xl font-headline mb-2 text-primary">Material:</h5>
-				<div class="flex flex-wrap lg:order-last mr-4 items-center">
+				<div class="flex flex-wrap gap-2 lg:order-last items-center">
 					{#each products[selectedModel].materials as _, i}
 						<button
 							on:click={() => updateSelectedMaterial(i)}
-							class="mx-2 shadow-lg h-14 w-14 transform-gpu hover:translate-y-1 ease-in duration-100 mt-4 {selectedMaterial ==
+							class="shadow-lg h-14 w-14 transform-gpu hover:translate-y-1 ease-in duration-100 mt-4 {selectedMaterial ==
 							i
 								? 'border-4 border-primary'
 								: 'border-none'}"
@@ -211,8 +235,8 @@
 				</div>
 			</div>
 
-			<div class="prose font-paragraph sm:mx-4 lg:mx-2">
-				<h2>{products[selectedModel].name.replace(/([A-Z])/g, ' $1').trim()}</h2>
+			<div class="prose font-paragraph">
+				<h2>{addSpaceBetweenCapitals(products[selectedModel].name)}</h2>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus magni enim accusamus
 					tenetur saepe ducimus, blanditiis alias animi fuga quas vitae consectetur obcaecati
